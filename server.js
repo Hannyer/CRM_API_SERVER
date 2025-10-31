67fa4b514ce3da62ab63189bf471429de9f56bf8
Hannyer <57728626+Hannyer@users.noreply.github.com>
2025-10-31 01:02:14 -0600

    update

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 ﻿require('dotenv').config();
 const app = require('./app');
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger.json');
 
 const PORT = process.env.PORT || 3000;
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
